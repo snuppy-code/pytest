f90ee2fba48418eaa6b7f5c75c4d8cc4881ff795
anteater-limpness <bunyaminlkeser@gmail.com>
2026-02-06 23:59:01 +0100

    dherv

--- flake.nix
+++ flake.nix
@@ -12,20 +12,9 @@
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
     devShells."x86_64-linux".default = pkgs.mkShell {
-      packages = with pkgs; [(python311.withPackages (ps: [ps.pygame-ce]))];
+      packages = [
+        (pkgs.python314.withPackages (ps: with ps; [pygame-ce numpy]))
+        ];
     };
-
-    #pkgs.mkShell {
-    #      # could also be buildInputs,, for my purposes it do not matter: https://discourse.nixos.org/t/difference-between-buildinputs-and-packages-in-mkshell/60598/2
-    #      buildInputs = with pkgs; [
-    #        python315.withPackages
-    #        (ps: [
-    #          #pygame-ce
-    #          ps.numpy
-    #        ])
-    #      ];
-    #      # env variables..
-    #      #env.RUST_SRC_PATH = "${pkgs.rust.blablabla.rustLibSrc}";
-    #    };
   };
 }

25f7ac652f27f969bf1416e9542858cc2307f88b
snuppy <snuppy.code+git@pm.me>
2026-02-07 12:07:19 +0100

    Update loadingscreen.py

--- src/scenes/loadingscreen.py
+++ src/scenes/loadingscreen.py
@@ -13,23 +13,29 @@ class LoadingScreen:
         s = self.loading_font.render(t,True,(255,255,255))
         self.initial_w = s.width
         self.initial_h = s.height
-        
+    
+    def onEnter(self):
+        # Loading is expected to be very short,
+        # just draw black screen so we don't flash "loading!" for 1 microsecond
+        self.ctx.screen.fill("black")
+        self.ctx.audios.load()
         
     def onFrame(self):
-        w = self.ctx.w
-        h = self.ctx.h
-        dt_s = self.ctx.dt_s
+        pass
+        # w = self.ctx.w
+        # h = self.ctx.h
+        # dt_s = self.ctx.dt_s
         
-        if self.loading_progress >= 100:
-            self.ctx.transition_scene_to("MainMenu")
-        else:
-            self.loading_progress += 60 * self.ctx.dt_s
+        # if self.loading_progress >= 100:
+        #     self.ctx.transition_scene_to("MainMenu")
+        # else:
+        #     self.loading_progress += 60 * self.ctx.dt_s
         
-        self.ctx.screen.fill("black")
+        # self.ctx.screen.fill("black")
         
-        print("LoadingScene onFrame called!")
+        # print("LoadingScene onFrame called!")
         
-        t = f"Loading progress: {self.loading_progress:.2f}%"
-        s = self.loading_font.render(t,True,(255,255,255))
-        p = (self.ctx.w/2-self.initial_w/2,self.ctx.h/2-self.initial_h/2)
-        self.ctx.screen.blit(s,p)
+        # t = f"Loading progress: {self.loading_progress:.2f}%"
+        # s = self.loading_font.render(t,True,(255,255,255))
+        # p = (self.ctx.w/2-self.initial_w/2,self.ctx.h/2-self.initial_h/2)
+        # self.ctx.screen.blit(s,p)

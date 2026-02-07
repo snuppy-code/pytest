63b06c12d5369e156edfdb5cc1a512128b0af8cc
anteater-limpness <bunyaminlkeser@gmail.com>
2026-02-07 17:10:12 +0100

    Update camp.py

--- src/scenes/camp.py
+++ src/scenes/camp.py
@@ -1,29 +1,30 @@
-# import pygame
-# import pygame_gui # type: ignore
+import pygame
+import pygame_gui # type: ignore
 
-# class MainMenu:
-#     def __init__(self,ctx):
-#         self.ctx = ctx
-    
-#     def onEnter(self):
-#         pass
+class Camp:
+    def __init__(self,ctx):
+        self.ctx = ctx
 
-#     def onExit(self):
-#         pass
+    def onEnter(self):
+        pass
+
+    def onExit(self):
+        pass
+
+    def onFrame(self,events):
+        w = self.ctx.w
+        h = self.ctx.h
+        dt_s = self.ctx.dt_s
     
-#     def onFrame(self,events):
-#         w = self.ctx.w
-#         h = self.ctx.h
-#         dt_s = self.ctx.dt_s
-        
-        
-#         # replace with background image 
-#         self.ctx.screen.fill((195, 176, 165))
-        
-#         menu = pygame.Surface((self.ctx.w,self.ctx.h))
-        
-#         pygame.draw.rect(menu,(222, 212, 203),pygame.Rect(self.ctx.w/2-400/2,60+(150+20)*0,400,150),0,40)
-#         pygame.draw.rect(menu,(222, 212, 203),pygame.Rect(self.ctx.w/2-400/2,60+(150+20)*1,400,150),0,40)
-#         pygame.draw.rect(menu,(222, 212, 203),pygame.Rect(self.ctx.w/2-400/2,60+(150+20)*2,400,150),0,40)
-        
-#         self.ctx.screen.blit(menu)
+    
+        # replace with background image 
+        self.ctx.screen.fill((195, 176, 165))
+    
+        menu = pygame.Surface((self.ctx.w,self.ctx.h))
+    
+        pygame.draw.rect(menu,(222, 212, 203),pygame.Rect(self.ctx.w/2-400/2,60+(150+20)*0,400,150),0,40)
+        pygame.draw.rect(menu,(222, 212, 203),pygame.Rect(self.ctx.w/2-400/2,60+(150+20)*1,400,150),0,40)
+        pygame.draw.rect(menu,(222, 212, 203),pygame.Rect(self.ctx.w/2-400/2,60+(150+20)*2,400,150),0,40)
+
+        self.ctx.screen.blit(menu)    
+        self.ctx.screen.blit(menu)

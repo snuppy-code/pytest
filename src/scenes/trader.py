1f32cf8d2ea1c00fab628a3f684a0baf859f2e10
anteater-limpness <bunyaminlkeser@gmail.com>
2026-02-08 21:57:34 +0100

    Update trader.py

--- src/scenes/trader.py
+++ src/scenes/trader.py
@@ -5,16 +5,13 @@ import random
 class Trader:
     def __init__(self, ctx):
         self.ctx = ctx
+        self.to_draw = {}
 
     def onEnter(self):
         blueberry_seed_image = self.ctx.images["blueberry_seedbag.png"]
         money_image = self.ctx.images["blueberry_seedbag.png"]
         potato_image = self.ctx.images["potato_growing_grown.png"]
         daikon_image = self.ctx.images["daikon_growing_grown.png"]
-<<<<<<< Updated upstream
-        # camp
-=======
->>>>>>> Stashed changes
 
         offer_type = random.randint(1,2)
         amount = 0
@@ -23,6 +20,10 @@ class Trader:
         else:
             amount = random.randint(1,5)
 
+        if offer_type == 1:
+            self.to_draw["blueberry_seedbag.png"] = amount
+        else:
+            self.to_draw["blueberry_seedbag.png"] = amount
 
     def onExit(self):
         pass
@@ -35,6 +36,9 @@ class Trader:
             if e.type == pygame.KEYDOWN and e.key == pygame.K_e:
                 print("Detected PLAYER pressed E")
                 self.ctx.transition_scene_to("Camp")
+
+        for img in self.to_draw:
+            self.ctx.vscreen.blit(self.ctx.images[img], dest=(500,200))
         
     
     def alwaysTick(self, events):
